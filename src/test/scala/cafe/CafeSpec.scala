2a02444fe9e7f91efac215158517577e0ef90ceb
Adam Conder <adamdconder@gmail.com>
2017-12-20 15:18:48 +0000

    Add test for boiling water

---
+++ src/test/scala/cafe/CafeSpec.scala
@@ -0,0 +1,22 @@
+package cafe
+
+import org.scalatest.{MustMatchers, WordSpec}
+
+class CafeSpec extends WordSpec with MustMatchers {
+
+  "Cafe" when {
+
+    "boiling water" should {
+
+      "boil water to 40 degrees" in {
+        val temperature = 40D
+        val water = Water(temperature = 0)
+        val boiled = Cafe.boil(water)
+        boiled.temperature mustEqual 40D
+      }
+
+    }
+
+  }
+
+}
