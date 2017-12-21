206a640d5b9fd43b8fad17c2661b0541006499ef
Adam Conder <adamdconder@gmail.com>
2017-12-21 14:25:39 +0000

    Asserting coffee temperature reduces 5 degrees

--- src/test/scala/models/CoffeeSpec.scala
+++ src/test/scala/models/CoffeeSpec.scala
@@ -7,10 +7,11 @@ class CoffeeSpec extends WordSpec with MustMatchers {
 
   "Coffee" should {
 
-    "be able to have milk added" in {
+    "be able to have milk added and reduce the temperature 5 degrees" in {
       val coffeeWithoutMilk = Coffee(temperature = 40D, ground = GroundCoffee("Arrabica"))
       val coffeeWithMilk = coffeeWithoutMilk.addMilk(FrothedMilk("Whole"))
       coffeeWithMilk.hasMilk mustBe true
+      coffeeWithMilk.temperature mustBe 35D
     }
 
   }
