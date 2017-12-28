ef402428ca2c14398b5eaa0174a943b4fcdaf7a1
jennygrahamjones <jenny.grahamjones@gmail.com>
2017-12-28 09:52:14 +0000

    Refactor to use Futures

--- src/main/scala/cafe/Cafe.scala
+++ src/main/scala/cafe/Cafe.scala
@@ -2,24 +2,27 @@ package cafe
 
 import cafe.models._
 
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
 object Cafe {
 
-  def heat(water: Water, temperature : Double = 40D) : Water = water.copy(temperature)
+  def heat(water: Water, temperature : Double = 40D) : Future[Water] = Future { water.copy(temperature) }
 
-  def grind(beans: CoffeeBeans) : GroundCoffee = GroundCoffee(brand = beans.brand)
+  def grind(beans: CoffeeBeans) : Future[GroundCoffee] = Future { GroundCoffee(brand = beans.brand) }
 
-  def frothMilk(milk: Milk) : FrothedMilk = {
+  def frothMilk(milk: Milk) : Future[FrothedMilk] = Future {
     require(!milk.equals(SemiSkimmedMilk))
     FrothedMilk(milk.`type`)
   }
 
   case class BrewingException() extends Exception("Water is too cold")
 
-  def brew(water: Water, coffee: GroundCoffee) : Coffee = {
+  def brew(water: Water, coffee: GroundCoffee) : Future[Coffee] = Future {
     if (water.temperature < 40D) {
       throw BrewingException()
     } else {
-      Coffee(water.temperature, coffee)
+     Coffee(water.temperature, coffee)
     }
   }
 
